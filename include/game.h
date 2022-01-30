6a9f3fc3768cf48a48c9f86254c20a12c57900b4
akcube <a.kishorekumar@students.iiit.ac.in>
2022-01-30 20:25:50 +0530

    Added textures

--- include/game.h
+++ include/game.h
@@ -3,6 +3,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <string>
 
 enum GameState {
     GAME_ACTIVE,
@@ -12,6 +13,9 @@ enum GameState {
 
 class Game
 {
+private:
+    void loadTextures();
+
 public:
     GameState               State;	
     bool                    Keys[1024];
@@ -24,6 +28,25 @@ public:
     void ProcessInput(float dt);
     void Update(float dt);
     void Render();
+};
+
+const std::string texturesList[][2] = {
+    { "textures/black_queen.png", "bqueen" },
+    { "textures/black_rook.png", "brook" },
+    { "textures/black_bishop.png", "bbishop" },
+    { "textures/black_knight.png", "bknight" },
+    { "textures/black_king.png", "bking" },
+    { "textures/black_pawn.png", "bpawn" },
+    { "textures/white_queen.png", "wqueen" },
+    { "textures/white_rook.png", "wrook" },
+    { "textures/white_bishop.png", "wbishop" },
+    { "textures/white_knight.png", "wknight" },
+    { "textures/white_king.png", "wking" },
+    { "textures/white_pawn.png", "wpawn" },
+    { "textures/brown_dark_square.png", "brown_dark_square" },
+    { "textures/brown_light_square.png", "brown_light_square" },
+    { "textures/gray_dark_square.png", "gray_dark_square" },
+    { "textures/gray_light_square.png", "gray_light_square" },
 };
 
 #endif
--- src/game.cpp
+++ src/game.cpp
@@ -1,4 +1,9 @@
 #include "game.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+
+// Game-related State data
+SpriteRenderer  *Renderer;
 
 Game::Game(unsigned int width, unsigned int height) 
     : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -8,12 +13,24 @@ Game::Game(unsigned int width, unsigned int height)
 
 Game::~Game()
 {
-    
+    delete Renderer;
 }
 
 void Game::Init()
 {
-   
+    // load shaders
+    ResourceManager::LoadShader("shaders/sprite.vs", "shaders/sprite.frag", nullptr, "sprite");
+    // configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width), 
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // set render-specific controls
+    Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+    // load textures
+    int num_textures = sizeof(texturesList)/sizeof(texturesList[0]);
+    for(int i=0; i<num_textures; i++)
+        ResourceManager::LoadTexture(texturesList[i][0].c_str(), true, texturesList[i][1].c_str());        
 }
 
 void Game::Update(float dt)
@@ -28,5 +45,6 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
-    
+        Renderer->DrawSprite(ResourceManager::GetTexture("gray_light_square"), 
+                glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
 }
